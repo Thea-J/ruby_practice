2b9f913a71c5c60823686b2cbe2c57e6adf85233
github username <github email address>
2020-10-25 18:15:45 +0100

    two-sum partial solution
    Returns the indices of the addends but the indices are not unique

--- two_sum.rb
+++ two_sum.rb
@@ -1,1 +1,18 @@
-"Hello World"
+#Find 2 integers in the nums array whose sum is target
+#return their unique indices in an array
+
+def two_sum(nums, target)
+ 
+#Define an empty array that will store the solution
+  solution = [ ];
+ 
+#Find the combination of nums elements that sum to target & store in an array
+  addends = nums.combination(2).find{|combo_array| combo_array.sum == target }
+   
+#Find the indices of the addends from the nums array and push them into the solutions array 
+  addends.length.times {|element| solution.push(nums.index{|int| addends[element] == int })}
+
+#Implicitly return the solution
+  solution
+
+end

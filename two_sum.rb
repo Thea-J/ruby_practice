780262b53e21c8f74cf2e56734b50a81ee8fbb48
github username <github email address>
2020-10-25 19:19:45 +0100

    returns unique indices
    Faster runtime required

--- two_sum.rb
+++ two_sum.rb
@@ -8,11 +8,17 @@ def two_sum(nums, target)
  
 #Find the combination of nums elements that sum to target & store in an array
   addends = nums.combination(2).find{|combo_array| combo_array.sum == target }
-   
+
+#Check if the addends array contains duplicate values  
 #Find the indices of the addends from the nums array and push them into the solutions array 
-  addends.length.times {|element| solution.push(nums.index{|int| addends[element] == int })}
+if addends.uniq.length == addends.length
+  addends.length.times {|element| nums.each_with_index{|value, index| value==addends[element]? solution.push(index):nil } }
+else 
+  1.times {|element| nums.each_with_index{|value, index| value==addends[element]? solution.push(index):nil } }
+end
 
 #Implicitly return the solution
   solution
 
-end
+end 
+

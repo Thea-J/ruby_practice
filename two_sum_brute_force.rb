744e0bf590abb4587e75869ca2aa34b8313ec309
github username <github email address>
2020-10-26 21:14:31 +0100

    two_sum brute force approach completed
    Selection algorithm: Time complexity - O(n^2), Space complexity: O(1)

--- two_sum_brute_force.rb
+++ two_sum_brute_force.rb
@@ -1,20 +1,21 @@
-require "pry"
+require 'pry'
 #Find 2 integers in the nums array whose sum is target
 #return their unique indices in an array
 
 def two_sum(nums, target)
- solution = []
- for i in 0...nums.length
-    j = i+1
- binding.pry
-    break if j=nums.length
-    nums[i] + nums[j] == target ? solution = [i, j] : j+=1
-  end
- solution
-end    
+    solution = []
+    i = 0
+    #Example: i=0, j=1, 2, 3, 4
+    while i < nums.length - 1
+       j = i+1
+       while j < nums.length
+         if nums[i] + nums[j] == target 
+           return solution.push(i, j)
+         end
+        j+=1
+        end
+        i+=1
+    end
+end  
 
-two_sum([2,7,11,15], 9)
-
-#two_sum([1, 3, 4, 3, 7], 6)
-# nums = [1, 3, 4, 3, 7] target = 6
-
+#   binding.pry

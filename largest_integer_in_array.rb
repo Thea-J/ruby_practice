0cfbaa03ff6aa475ddf245a1ddb3915f64316f7e
github username <63208817+Thea-J@users.noreply.github.com>
2020-10-30 18:58:11 +0100

    completed brute force approach

--- largest_integer_in_array.rb
+++ largest_integer_in_array.rb
@@ -8,14 +8,17 @@ end
 
 #Use a brute force algorithm to find the largest integer from a given array
 def largest_integer_brute_force_approach(array)
-    #Find the smallest possible integer value that can be in the array
-    inspector = array.sort[0] - 1
-
-    #Iterate over all the elements in the array
-    #Compare each element to the inspector 
-    #if a value is bigger than the inspector, that is the new inspector
-    #end once we've reached the final element in the array
+    inspector = array.sort[0] - 1  #Find the smallest possible integer value that can be in the array
+    index = 0 
+    while index < array.length do #Iterate over all the elements in the array
+        if array[index] > inspector  #if an array element is bigger than the inspector
+            inspector = array[index] #Reassign the value of the inspector to be said element
+        end
+        index+=1
+    end
+    inspector #Return the final value of the inspector
 end
+
 
 #Example:
 # example_array = [71, -9, 15, 23, -82, 100] 

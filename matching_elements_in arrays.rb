2440698bce37eca4789189e2a30cd42665d788b5
github username <63208817+Thea-J@users.noreply.github.com>
2020-10-30 20:48:00 +0100

    plan for brute force solution

---
+++ matching_elements_in arrays.rb
@@ -0,0 +1,14 @@
+#Given 2 arrays, return the matching elements
+
+def matching_elements_brute_force(array1, array2)
+
+#iterate over array1
+#For each element in array 1, check if it is equal to any element in array2
+#Return the unique array of matching elements 
+
+end
+
+#Example: 
+# array1 = [3, 6, 9, 12, 15]
+# array2 = [4, 6, 12, 16, 21]
+# returns [6, 12]

11a13b2ece456855cc9444d27ae889384e644c94
github username <63208817+Thea-J@users.noreply.github.com>
2021-03-17 01:19:27 +0100

    assessed requirements: main & edge cases

--- spec/count_bits_spec.rb
+++ spec/count_bits_spec.rb
@@ -1,4 +1,11 @@
 #Challenge: 
-#Write a method that accepts a non-negative integer
-#Calculates the binary representation of that integer
-#Returns the count of the bits (1's) in the binary representation
+# Write a method that accepts a non-negative integer
+# Calculates the binary representation of that integer
+# Returns the count of the bits (1's) in the binary representation
+
+#Requirements:
+# Main cases: +ve integer
+# Edge cases: integer 0, 
+             #integer whose binary representation is only 0's, 
+             #integer whose binary representation is only 1's
+

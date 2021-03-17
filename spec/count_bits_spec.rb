30eb97cb6e61774efa43c0b042683d2909e37487
github username <63208817+Thea-J@users.noreply.github.com>
2021-03-17 01:15:46 +0100

    define the method count_bits

---
+++ spec/count_bits_spec.rb
@@ -0,0 +1,4 @@
+#Challenge: 
+#Write a method that accepts a non-negative integer
+#Calculates the binary representation of that integer
+#Returns the count of the bits (1's) in the binary representation
